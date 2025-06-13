c3593a087a524f02a5bc089bc3e9c0c24302d44a
Vishnu Priyan S S <vishnu32510@gmail.com>
2025-06-13 07:39:36 -0500

    Initial Commit

---
+++ MobileWeatherApp/MobileWeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MobileWeatherAppApp.swift
+//  MobileWeatherApp
+//
+//  Created by Vishnu Priyan Sellam Shanmugavel on 6/13/25.
+//
+
+import SwiftUI
+
+@main
+struct MobileWeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MobileWeatherAppTests/MobileWeatherAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  MobileWeatherAppTests.swift
+//  MobileWeatherAppTests
+//
+//  Created by Vishnu Priyan Sellam Shanmugavel on 6/13/25.
+//
+
+import Testing
+@testable import MobileWeatherApp
+
+struct MobileWeatherAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
